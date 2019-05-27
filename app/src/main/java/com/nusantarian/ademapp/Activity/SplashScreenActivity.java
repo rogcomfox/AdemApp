f4af8d48c0480d9288e9a1a347f972b4981b7681
ygladiensyah <ygladiensyah@gmail.com>
2019-05-27 21:37:55 +0700

    fix logo and add assets

--- app/src/main/java/com/nusantarian/ademapp/Activity/SplashScreenActivity.java
+++ app/src/main/java/com/nusantarian/ademapp/Activity/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package com.nusantarian.ademapp.Activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,14 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LandingPageActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
