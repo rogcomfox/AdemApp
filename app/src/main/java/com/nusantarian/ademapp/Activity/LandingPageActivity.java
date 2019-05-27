7d2935ecfcd215cee719e2bec1564365fd9eba7f
ygladiensyah <ygladiensyah@gmail.com>
2019-05-27 22:24:09 +0700

    fix UI authentication

--- app/src/main/java/com/nusantarian/ademapp/Activity/LandingPageActivity.java
+++ app/src/main/java/com/nusantarian/ademapp/Activity/LandingPageActivity.java
@@ -1,7 +1,10 @@
 package com.nusantarian.ademapp.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.nusantarian.ademapp.R;
 
@@ -11,5 +14,21 @@ public class LandingPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        Button btn_login = findViewById(R.id.btn_login);
+        Button btn_register = findViewById(R.id.btn_register);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
     }
 }
