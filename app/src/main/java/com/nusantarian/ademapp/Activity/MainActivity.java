6fe99b1a8eec6ff35cd0dae3c0f7c705f1511143
ygladiensyah <ygladiensyah@gmail.com>
2019-06-15 16:29:14 +0700

    add navigation drawer activity

--- app/src/main/java/com/nusantarian/ademapp/Activity/MainActivity.java
+++ app/src/main/java/com/nusantarian/ademapp/Activity/MainActivity.java
@@ -1,52 +1,90 @@
 package com.nusantarian.ademapp.Activity;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 
-import com.nusantarian.ademapp.Fragment.AchievementFragment;
-import com.nusantarian.ademapp.Fragment.ChatFragment;
-import com.nusantarian.ademapp.Fragment.HomeFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
 import com.nusantarian.ademapp.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    private final Fragment fragment1 = new HomeFragment();
-    private final Fragment fragment2 = new ChatFragment();
-    private final Fragment fragment3 = new AchievementFragment();
-    private final FragmentManager fragmentManager = getSupportFragmentManager();
-    Fragment active = fragment1;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        fragmentManager.beginTransaction().add(R.id.content_frame, fragment3, "3").hide(fragment3).commit();
-        fragmentManager.beginTransaction().add(R.id.content_frame, fragment2, "2").hide(fragment2).commit();
-        fragmentManager.beginTransaction().add(R.id.content_frame, fragment1, "1").commit();
-
-        BottomNavigationView bottom_nav = findViewById(R.id.bottom_nav);
-        bottom_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.nav_home:
-                        fragmentManager.beginTransaction().hide(active).show(fragment1).commit();
-                        break;
-                    case R.id.nav_chat:
-                        fragmentManager.beginTransaction().hide(active).show(fragment2).commit();
-                        break;
-                    case R.id.nav_achievement:
-                        fragmentManager.beginTransaction().hide(active).show(fragment3).commit();
-                        break;
-                }
-                return true;
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.setting_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()){
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
