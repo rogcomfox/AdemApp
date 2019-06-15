e62d14c4024ed3e854e555bb2050ad24f6b1763c
ygladiensyah <ygladiensyah@gmail.com>
2019-06-15 20:51:39 +0700

    fix main page

--- app/src/main/java/com/nusantarian/ademapp/Activity/MainActivity.java
+++ app/src/main/java/com/nusantarian/ademapp/Activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.nusantarian.ademapp.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,11 +18,26 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.nusantarian.ademapp.Fragment.AchievementFragment;
+import com.nusantarian.ademapp.Fragment.ChatFragment;
+import com.nusantarian.ademapp.Fragment.HomeFragment;
 import com.nusantarian.ademapp.R;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private final Fragment fragment1 = new HomeFragment();
+    private final Fragment fragment2 = new ChatFragment();
+    private final Fragment fragment3 = new AchievementFragment();
+    private final FragmentManager fm = getSupportFragmentManager();
+    private Fragment active = fragment1;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +45,10 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Home");
+
+        mAuth = FirebaseAuth.getInstance();
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -41,6 +56,32 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        fm.beginTransaction().add(R.id.frame_container, fragment3, "3").hide(fragment3).commit();
+        fm.beginTransaction().add(R.id.frame_container, fragment2, "2").hide(fragment2).commit();
+        fm.beginTransaction().add(R.id.frame_container,fragment1, "1").commit();
+
+        BottomNavigationView bottom_nav = findViewById(R.id.bottom_nav);
+        bottom_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        fm.beginTransaction().hide(active).show(fragment1).commit();
+                        Objects.requireNonNull(getSupportActionBar()).setTitle("Home");
+                        active = fragment1;break;
+                    case R.id.nav_chat:
+                        fm.beginTransaction().hide(active).show(fragment2).commit();
+                        Objects.requireNonNull(getSupportActionBar()).setTitle("Chat");
+                        active = fragment2;break;
+                    case R.id.nav_trophy:
+                        fm.beginTransaction().hide(active).show(fragment3).commit();
+                        Objects.requireNonNull(getSupportActionBar()).setTitle("Achievement");
+                        active = fragment3;break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
@@ -65,11 +106,12 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Toast.makeText(this, "Under Construction", Toast.LENGTH_SHORT).show();break;
+            case R.id.nav_search:
+                SearchView searchView =(SearchView)item.getActionView();
+                searchView.setQueryHint("Search");break;
         }
 
         return super.onOptionsItemSelected(item);
@@ -80,7 +122,11 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         switch (item.getItemId()){
-
+            case R.id.nav_profile:
+                Toast.makeText(this, "Under Construction", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_info:
+                startActivity(new Intent(MainActivity.this, InfoActivity.class));break;
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
